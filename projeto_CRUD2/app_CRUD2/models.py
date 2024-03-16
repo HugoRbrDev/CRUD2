363a800b01f72ef31ce3cb0ced9ba9329e8c4393
HugoRbrDev <hugorbrdev@gmail.com>
2024-03-16 02:44:44 -0300

    Banco de Dados testado

--- projeto_CRUD2/app_CRUD2/models.py
+++ projeto_CRUD2/app_CRUD2/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class bancoDeDados(models.Model):
+class BancoDeDados(models.Model):
 
     title = models.CharField(max_length=100, null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
--- projeto_CRUD2/app_CRUD2/views.py
+++ projeto_CRUD2/app_CRUD2/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from .models import BancoDeDados
+
 
 def home(request):
-    return render(request, "paginas/home.html")
+    
+    bancoDeDados_variavel = BancoDeDados.objects.all()
+    return render(request, "paginas/home.html", {"bancoDeDados_obj": bancoDeDados_variavel})
